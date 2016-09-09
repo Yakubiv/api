c8e0d73fca867a1ae44d0a1be901a1ffab31b7db
Petro <petro.yakubiv@gmail.com>
2016-09-09 17:26:24 +0300

    My first Phoenix API

---
+++ config/prod.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+config :peepchat, Peepchat.Endpoint,
+  http: [port: {:system, "PORT"}],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "immense-sierra-92111.herokuapp.com", port: 443],
+
+
+config :peepchat, Peepchat.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
+
+config :logger, level: :info
+
+import_config "prod.secret.exs"
