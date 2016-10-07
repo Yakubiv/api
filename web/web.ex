927f36fa81a673e9e6d7ecad87b06fee496ddc29
Petro <petro.yakubiv@gmail.com>
2016-10-07 14:50:41 +0300

    Add room entity to app

--- web/web.ex
+++ web/web.ex
@@ -1,20 +1,4 @@
 defmodule Peepchat.Web do
-  @moduledoc """
-  A module that keeps using definitions for controllers,
-  views and so on.
-
-  This can be used in your application as:
-
-      use Peepchat.Web, :controller
-      use Peepchat.Web, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below.
-  """
 
   def model do
     quote do
